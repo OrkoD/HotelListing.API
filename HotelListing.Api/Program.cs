23364627ecc22910c48ce74d6dcb58cf93839fc8
OrkoD <drozda.orest@gmail.com>
2026-08-18 10:57:00 +0300

    Configure SQL Server connection string and register HotelListingDbContext

--- HotelListing.Api/Program.cs
+++ HotelListing.Api/Program.cs
@@ -1,8 +1,14 @@
+using HotelListing.Api.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<HotelListingDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("HotelListingDbConnectionString")));
 
 var app = builder.Build();
 
